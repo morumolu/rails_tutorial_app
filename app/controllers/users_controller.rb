087bd32673bf2ebbcd9154fb5f5a4118d4ba3e18
morumolu <nh40875@gmail.com>
2020-05-21 07:33:47 +0000

    add Puma Procfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    # if @user.save
-    if false
+    if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
